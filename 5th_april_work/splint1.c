a5d7ed5b1ce0915d6bbb26913069f395265aec97
Yuvraj Singh Solanki <98091421+YuvrajSingh1102@users.noreply.github.com>
2022-04-18 00:38:16 +0530

    Delete 5th_april_work directory

--- 5th_april_work/splint1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    int i = 5;
-    printf("%d\n value is", i);
-
-}
--- 5th_april_work/splint7.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main() {
-    char x[] = "Kernel";
-    printf("%d\n", x);
-    return 0;
-}
